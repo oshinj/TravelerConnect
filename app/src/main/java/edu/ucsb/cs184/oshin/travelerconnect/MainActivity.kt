5f9f78a871bc065ed8e57a028caa8b2ac20dad60
oshinj <45084634+oshinj@users.noreply.github.com>
2019-12-03 14:40:33 -0800

    Initial commit

---
+++ app/src/main/java/edu/ucsb/cs184/oshin/travelerconnect/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.ucsb.cs184.oshin.travelerconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
