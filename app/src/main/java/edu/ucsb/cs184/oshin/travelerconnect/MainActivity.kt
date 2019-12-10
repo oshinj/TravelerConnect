32750127f1d09694be4593756454adba080489e8
fliang <fliang@umail.ucsb.edu>
2019-12-09 16:46:49 -0800

    First commit to branch fujie. Added features: Matched Users.

--- app/src/main/java/edu/ucsb/cs184/oshin/travelerconnect/MainActivity.kt
+++ app/src/main/java/edu/ucsb/cs184/oshin/travelerconnect/MainActivity.kt
@@ -1,12 +1,34 @@
 package edu.ucsb.cs184.oshin.travelerconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+/*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}*/
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenActivity : Button = findViewById<Button>(R.id.Login)
+        btnOpenActivity.setOnClickListener{
+                // Handler code here.
+                val intent = Intent(this, Main_UserMenu::class.java);
+                startActivity(intent);
+
+            print("clicked login");
+        }
+
     }
 }
